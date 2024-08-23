147340363923cf0a5e8dc99ec16a080648ab3fb9
Shefali1499 <shefalisawant1499@gmail.com>
2024-08-24 00:35:21 +0530

    portfolio created

---
+++ src/constants/images.js
@@ -0,0 +1,49 @@
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+import reactLogo from '../assets/reactLogo.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import mu5 from '../assets/mu5.png';
+import node from '../assets/node.png';
+import flutter from '../assets/flutter.png';
+import redux from '../assets/redux.png';
+import next from '../assets/next.png';
+import sass from '../assets/sass.png';
+import typescript from '../assets/typescript.png';
+import vue from '../assets/vue.png';
+import pc from '../assets/pc.png';
+
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+
+import profile from '../assets/hero3.png';
+import circle from '../assets/circle.svg';
+import logo from '../assets/home.png';
+
+
+export const Images={
+  email,
+  mobile,
+  pc,
+  reactLogo,
+  next,
+  html,
+  javascript,
+  flutter,
+  mu5,
+  node,
+  redux,
+  sass,
+  typescript,
+  vue,
+  about01,
+  about02,
+  about03,
+  about04,
+  profile,
+  circle,
+  logo,
+};
+export default Images;
